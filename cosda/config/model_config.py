fd1ce21bde5daff08ad016592eeced29ee00b22d
ZHOURui6025 <118749835+ZHOURui6025@users.noreply.github.com>
2025-04-02 13:05:35 +0800

    Update model_config.py

--- cosda/config/model_config.py
+++ cosda/config/model_config.py
@@ -38,7 +38,7 @@ def build_args():
     parser.add_argument("--note", default="COSDA")
     parser.add_argument('--int_epsilon', type=float, default=0.1, help='L_sta intervention below bound (default: 0.1)')
     parser.add_argument('--lambda_sta', type=float, default=0.001, help='hyperparameter for loss-sta weight (default: 1)')
-    parser.add_argument('--lambda_beta_e', type=float, default=1, help='hyperparameter for loss-beta weight (default: 1)')
+    parser.add_argument('--lambda_beta_e', type=float, default=0.2, help='hyperparameter for loss-beta weight (default: 1)')
     parser.add_argument('--lambda_beta_d', type=float, default=1, help='hyperparameter for loss-beta weight (default: 1)')
     parser.add_argument('--lambda_int', type=float, default=0.0001, help='hyperparameter for sus (default: 0.001)')
     # parser.add_argument('--lambda_reg', type=float, default=0.1, help='hyperparameter for intervention (default: 0.1)')
